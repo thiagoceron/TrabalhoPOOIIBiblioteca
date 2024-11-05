e4ec516079486d4a9cf65ed97617a18a16746968
thiagoceron <139267195+thiagoceron@users.noreply.github.com>
2024-11-05 19:34:21 -0300

    Update IUsuario.java

--- src/main/java/org/exemple/biblioteca/dao/IUsuario.java
+++ src/main/java/org/exemple/biblioteca/dao/IUsuario.java
@@ -1,8 +1,15 @@
 package org.exemple.biblioteca.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 import org.exemple.biblioteca.model.Usuario;
 
-public interface IUsuario { // Declara a interface IUsuario, que define métodos para operações em usuários.
-    void inserir(Usuario usuario) throws SQLException; // Declara o método de inserção de um usuário.
+public interface IUsuario {
+    void inserir(Usuario usuario) throws SQLException; // Método para inserir um usuário.
+    List<Usuario> buscarTodos() throws SQLException; // Método para buscar todos os usuários.
+    void deletar(int usuarioID) throws SQLException; // Método para deletar um usuário pelo ID.
+    List<Usuario> searchByName(String searchTerm) throws SQLException; // Método para buscar usuários pelo nome.
+    boolean usuarioExiste(int usuarioID) throws SQLException; // Método para verificar se o usuário existe.
+    int obterProximoUsuarioID() throws SQLException; // Método para obter o próximo ID disponível.
+    void atualizar(Usuario usuario) throws SQLException; // Método para atualizar um usuário.
 }
