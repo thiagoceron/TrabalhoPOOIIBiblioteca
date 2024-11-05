dbf56b19f657d180b7a3941e62285f02b1f0563e
thiagoceron <139267195+thiagoceron@users.noreply.github.com>
2024-11-05 19:34:32 -0300

    Update ILivro.java

--- src/main/java/org/exemple/biblioteca/dao/ILivro.java
+++ src/main/java/org/exemple/biblioteca/dao/ILivro.java
@@ -1,9 +1,13 @@
 package org.exemple.biblioteca.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 import org.exemple.biblioteca.model.Livro;
 
-public interface ILivro { // Declara a interface ILivro, que define métodos para operações em livros.
-    void inserir(Livro livro) throws SQLException; // Declara o método de inserção de um livro.
-    int obterProximoLivroID() throws SQLException; // Declara o método para obter o próximo ID disponível.
+public interface ILivro {
+    void inserir(Livro livro) throws SQLException; // Método para inserir um livro.
+    List<Livro> buscarTodos() throws SQLException; // Método para buscar todos os livros.
+    void deletar(int livroID) throws SQLException; // Método para deletar um livro pelo ID.
+    void atualizar(Livro livro) throws SQLException; // Método para atualizar informações do livro.
+    int obterProximoLivroID() throws SQLException; // Método para obter o próximo ID disponível.
 }
