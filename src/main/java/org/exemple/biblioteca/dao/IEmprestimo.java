a35a4ff2592036e143244f7d5c5edb0410f31bce
thiagoceron <139267195+thiagoceron@users.noreply.github.com>
2024-11-05 19:34:43 -0300

    Update IEmprestimo.java

--- src/main/java/org/exemple/biblioteca/dao/IEmprestimo.java
+++ src/main/java/org/exemple/biblioteca/dao/IEmprestimo.java
@@ -1,8 +1,11 @@
 package org.exemple.biblioteca.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 import org.exemple.biblioteca.model.Emprestimo;
 
-public interface IEmprestimo { // Declara a interface IEmprestimo, que define métodos para operações em empréstimos.
-    void inserir(Emprestimo emprestimo) throws SQLException; // Declara o método de inserção de um empréstimo.
+public interface IEmprestimo {
+    void inserir(Emprestimo emprestimo) throws SQLException; // Método para inserir um empréstimo.
+    List<Emprestimo> buscarTodos() throws SQLException; // Método para buscar todos os empréstimos.
+    void deletar(int id) throws SQLException; // Método para deletar um empréstimo pelo ID.
 }
