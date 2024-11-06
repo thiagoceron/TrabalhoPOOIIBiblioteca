34d06cb605619b66f4f900068cdc4585d5ada8c3
thiagoceron <139267195+thiagoceron@users.noreply.github.com>
2024-11-06 10:57:25 -0300

    Atualizar o IUsuario.java

--- src/main/java/org/exemple/biblioteca/dao/IUsuario.java
+++ src/main/java/org/exemple/biblioteca/dao/IUsuario.java
@@ -8,7 +8,6 @@ public interface IUsuario {
     void inserir(Usuario usuario) throws SQLException; // Método para inserir um usuário.
     List<Usuario> buscarTodos() throws SQLException; // Método para buscar todos os usuários.
     void deletar(int usuarioID) throws SQLException; // Método para deletar um usuário pelo ID.
-    List<Usuario> searchByName(String searchTerm) throws SQLException; // Método para buscar usuários pelo nome.
     boolean usuarioExiste(int usuarioID) throws SQLException; // Método para verificar se o usuário existe.
     int obterProximoUsuarioID() throws SQLException; // Método para obter o próximo ID disponível.
     void atualizar(Usuario usuario) throws SQLException; // Método para atualizar um usuário.
